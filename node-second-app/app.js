ecc43d2d618f482c86806e317e50b6815b575543
Christopher Steigerwald <cjsteigerwald@gmail.com>
2021-08-30 10:07:48 -0700

    Playing with template engines

--- node-second-app/app.js
+++ node-second-app/app.js
@@ -1,9 +1,34 @@
-const http = require('http');
+const express = require('express');
+const path = require('path');
 
-const routes = require('./routes');
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.engine(
+  'hbs',
+  expressHbs({
+    layoutsDir: 'views/layouts',
+    defaultLayout: 'main-layout',
+    extname: 'hbs',
+  }),
+);
+app.set('view engine', 'hbs');
+app.set('views', 'views');
 
-server.listen(3000);
+app.use(express.static(path.join(__dirname, 'public')));
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
+
+app.listen(3000);
