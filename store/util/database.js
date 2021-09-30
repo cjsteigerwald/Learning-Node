27dcd7c9806e31d358b8090f64b217b8a62cfff3
Christopher Steigerwald <cjsteigerwald@gmail.com>
2021-09-30 05:10:04 -0700

    Adding sequelize, and refactroing code

--- store/util/database.js
+++ store/util/database.js
@@ -1,10 +1,18 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize').Sequelize;
 
-const pool = mysql.createPool({
+const sequelize = new Sequelize('node-complete', 'root', '', {
+  dialect: 'mysql',
   host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: '',
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
+// const mysql = require('mysql2');
+
+// const pool = mysql.createPool({
+//   host: 'localhost',
+//   user: 'root',
+//   database: 'node-complete',
+//   password: '',
+// });
+
+// module.exports = pool.promise();
